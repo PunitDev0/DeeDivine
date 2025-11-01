c5c4812d74932742b5ffeaa7b01f72426d7e8d90
PunitDev0 <punitdeveloper1@gmail.com>
2025-11-01 18:11:38 +0530

    update meta tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="izQalQO3JpG14WdFrgz2-ngPvvDh2MB_Td3b2qtdyEw"/>
 
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
         <script>
